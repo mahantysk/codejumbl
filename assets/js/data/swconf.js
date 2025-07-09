50c066b745bb23f7f67e9f319440877d94e5942d
mahantysk <mahantysk@users.noreply.github.com>
2025-07-09 09:43:14 +0000

    deploy: d85f2abf15558bd67a434bea1a6878274fa6cc67

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752049240',resources: [
+    cacheName: 'chirpy-1752054193',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
